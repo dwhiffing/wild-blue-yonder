46780756ff64b434da37ab5e6b6b36af4ae1854c
Daniel Whiffing <danielwhiffing@gmail.com>
2020-08-22 16:01:17 -0400

    Fixes cursor movement

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,13 +1,11 @@
 const FISH_COLORS = [2, 4, 5, 6]
 
 const HOOKS = [
-  [1, 1, 1, 1, 1, 1, 1, 1, 1], // BOX
-  [0, 1, 0, 1, 0, 1, 0, 1, 0], // CROSS
-  [1, 0, 1, 0, 0, 0, 1, 0, 1], // CORNERS
-  [1, 1, 1, 0, 0, 0, 0, 0, 0], // TOP ROW
-  [0, 0, 0, 0, 0, 0, 1, 1, 1], // BOT ROW
-  [1, 0, 0, 1, 0, 0, 1, 0, 0], // LEFT COL
-  [0, 0, 1, 0, 0, 1, 0, 0, 1], // RIGHT COL
+  // [1, 1, 1, 1, 1, 1, 1, 1, 1], // BOX
+  [1, 0, 0, 1, 0, 0, 1, 0, 0], // COL
+  [0, 0, 1, 0, 1, 0, 1, 0, 0], // CORNERS
+  [1, 0, 0, 0, 1, 0, 0, 0, 1], // CORNERS
+  [1, 1, 1, 0, 0, 0, 0, 0, 0], // ROW
 ]
 
 export default class extends Phaser.Scene {
@@ -64,27 +62,42 @@ export default class extends Phaser.Scene {
     this.food.setFrame(Phaser.Math.RND.pick(FISH_COLORS) + foodType * 8)
     this.hookIndex = Phaser.Math.RND.between(0, HOOKS.length - 1)
     this.selectSprites()
+    this.validateHookPosition()
   }
 
   handleInput(event) {
+    const { width, height } = this.getHookDimensions()
     if (event.key === 'ArrowLeft' && this.hookCoord % 8 !== 0) {
       this.hookCoord--
       this.selectSprites()
     }
-    if (event.key === 'ArrowRight' && (this.hookCoord + 3) % 8 !== 0) {
+    if (event.key === 'ArrowRight' && (this.hookCoord + width) % 8 !== 0) {
       this.hookCoord++
       this.selectSprites()
     }
-    if (event.key === 'ArrowUp' && this.hookCoord > 8) {
+    if (event.key === 'ArrowUp' && this.hookCoord >= 8) {
       this.hookCoord -= 8
       this.selectSprites()
     }
-    if (event.key === 'ArrowDown' && this.hookCoord < 40) {
+    if (event.key === 'ArrowDown' && this.hookCoord < 56 - (height - 1) * 8) {
       this.hookCoord += 8
       this.selectSprites()
     }
     if (event.key === ' ') {
       this.submit()
+    }
+  }
+
+  validateHookPosition() {
+    const { width, height } = this.getHookDimensions()
+    const maxY = 56 - (height - 1) * 8
+    while (this.hookCoord - 1 > maxY) {
+      this.hookCoord -= 8
+      this.selectSprites()
+    }
+    while ((this.hookCoord % 8) + width > 8) {
+      this.hookCoord--
+      this.selectSprites()
     }
   }
 
@@ -99,7 +112,7 @@ export default class extends Phaser.Scene {
           ? 50
           : colorMatches || shapeMatches
           ? 10
-          : -10
+          : -25
       return sum + score
     }, 0)
     this.score += score
@@ -113,7 +126,8 @@ export default class extends Phaser.Scene {
   }
 
   selectSprites() {
-    this.sprites.forEach((sprite) => sprite.setAlpha(0.3))
+    this.sprites.forEach((sprite) => sprite.setAlpha(0.5))
+    // get the 9 tile box for hook starting from top left using hookCoord
     const hookCoordIndexes = [
       this.hookCoord,
       this.hookCoord + 1,
@@ -125,7 +139,9 @@ export default class extends Phaser.Scene {
       this.hookCoord + 17,
       this.hookCoord + 18,
     ]
+    // get the active tiles within that box
     const hookPatternIndexes = HOOKS[this.hookIndex]
+    // highlight those tiles
     const selected = this.sprites.filter((sprite, spriteIndex) =>
       hookCoordIndexes
         .filter((_, hookCoordIndex) => hookPatternIndexes[hookCoordIndex] === 1)
@@ -133,8 +149,22 @@ export default class extends Phaser.Scene {
     )
     selected.forEach((s) => s.setAlpha(1))
   }
+
+  getHookDimensions() {
+    const hook = HOOKS[this.hookIndex]
+    const cols = [
+      [hook[2], hook[5], hook[8]],
+      [hook[1], hook[4], hook[7]],
+      [hook[0], hook[3], hook[6]],
+    ]
+    const rows = [
+      [hook[6], hook[7], hook[8]],
+      [hook[3], hook[4], hook[5]],
+      [hook[0], hook[1], hook[2]],
+    ]
+    return {
+      width: 3 - cols.findIndex((col) => col.includes(1)),
+      height: 3 - rows.findIndex((row) => row.includes(1)),
+    }
+  }
 }
-// 00 01 02 03 04 05 06 07
-// 08 09 10 11 12 13 14 15
-// 16 17 18 19 20 21 22 23
-// 24 25 26 27 28 29 31 32
